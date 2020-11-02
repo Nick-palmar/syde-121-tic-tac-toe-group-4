597e9d76e6f7fc3ad068838840d90b2a67e9b463
Nick-palmar <66019522+Nick-palmar@users.noreply.github.com>
2020-11-02 17:04:00 -0500

    Update a4_q1.cpp
    first code commit

--- a4_q1.cpp
+++ a4_q1.cpp
@@ -1,1 +1,23 @@
+//***********************************************************
+// Student Name: 
+// Student Number: 
+//
+// SYDE 121 Lab: Home Assignment: 4, question 1
+// Filename: a4_q1
+// I hereby declare that this code, submitted for credit for the course
+// SYDE121, is a product of my own efforts.  This coded solution has
+// not been plagiarized from other sources and has not been knowingly
+// plagiarized by others
+//
+// Project: Tic Tac Toe
+// Purpose: Play tic tac toe with another person
+// Due Date: Friday, November 6, 2020
+//**************************************************************
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+
+}
