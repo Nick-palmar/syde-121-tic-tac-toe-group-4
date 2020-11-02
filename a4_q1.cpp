61752499c6c7723307b9838123df503e3e90e044
Nick-palmar <66019522+Nick-palmar@users.noreply.github.com>
2020-11-02 17:49:59 -0500

    Update a4_q1.cpp
    code outline

--- a4_q1.cpp
+++ a4_q1.cpp
@@ -14,13 +14,72 @@
 // Due Date: Friday, November 6, 2020
 //**************************************************************
 
-// please work
-
-
 #include <iostream>
 #include <vector>
+#include <string>
+
 using namespace std;
 
-int main () {
+bool check_if_winner(vector<vector<string>> board);
+// PURPOSE: 
+//     INPUTS: 
+//     OUTPUTS: 
+
+
+void output_board(vector<vector<string>> board);
+// PURPOSE: 
+//     INPUTS: 
+//     OUTPUTS: Nothing
+
+
+void update_board(vector<vector<string>>& board, char& current_player);
+// PURPOSE: 
+//     INPUTS: 
+//     OUTPUTS: Nothing
+
+
+void execute_tic_tac_toe();
+// PURPOSE: 
+
+
+bool check_if_winner(vector<vector<string>> board) {
 
 }
+
+
+void update_board(vector<vector<string>>& board, char& current_player) {
+
+}
+
+
+void output_board(vector<vector<string>> board) {
+
+}
+
+
+void execute_tic_tac_toe() {
+    // initialize and declare variables
+    vector<vector<string>> board = { {"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"} };
+    char starting_player = 'X';
+    char current_player = 'X';
+    bool winner = false;
+
+    do {
+        // take input
+        update_board(board, current_player);
+
+        // output board to console
+        output_board(board);
+
+        // check game board
+        winner = check_if_winner(board);
+    } while (!winner);
+}
+
+
+int main()
+{
+    execute_tic_tac_toe();
+}
+
+
