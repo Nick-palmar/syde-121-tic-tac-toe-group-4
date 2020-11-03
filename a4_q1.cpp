b9a6cd532ab1dd3c6b7c5e8f13a0930e354efdc8
Kevin Wang <70813550+kevinwang19@users.noreply.github.com>
2020-11-02 23:24:30 -0500

    Update a4_q1.cpp

--- a4_q1.cpp
+++ a4_q1.cpp
@@ -60,7 +60,7 @@ void update_board(vector<vector<string>>& board, char& current_player) {
         cout << "Enter a location to mark (1-16): ";
         cin >> input;
         
-        occupied = true && (board[(input - 1) / board[0].size()][(input - 1) % board[0].size()] == "X" || board[(input - 1) / 4][(input - 1) % 4] == "O");
+        occupied = true && (board[(input - 1) / board[0].size()][(input - 1) % board[0].size()] == "X" || board[(input - 1) / board[0].size()][(input - 1) % board[0].size()] == "O");
         
     } while (input < 1 || input > 16 || occupied);
     
