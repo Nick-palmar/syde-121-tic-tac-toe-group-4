139359eb4f4d5f1dfffba79d9727fbe5b11bcdec
Nick-palmar <66019522+Nick-palmar@users.noreply.github.com>
2020-11-03 00:14:51 -0500

    Update a4_q1.cpp
    output done

--- a4_q1.cpp
+++ a4_q1.cpp
@@ -27,9 +27,9 @@ bool check_if_winner(vector<vector<string>> board);
 
 
 void output_board(vector<vector<string>> board);
-// PURPOSE: 
-//     INPUTS: 
-//     OUTPUTS: Nothing
+// PURPOSE: Outputs the board to console to show occupied and unoccupied positions
+//     INPUTS: board - a 2D vector containing all the elements in the board as strings (numbers for empty spaces, X's, O's)
+//     OUTPUTS: Nothing; output to console
 
 
 void update_board(vector<vector<string>>& board, char& current_player);
@@ -42,30 +42,30 @@ void execute_tic_tac_toe();
 // PURPOSE: 
 
 
-bool check_if_winner(vector<vector<string>> board) {
-
-}
+//bool check_if_winner(vector<vector<string>> board) {
+//
+//}
 
 
 void update_board(vector<vector<string>>& board, char& current_player) {
     int input = 0;
     bool occupied = false;
-    
+
     do {
-        
+
         if (occupied) {
             cout << "Location is occupied." << endl;
         }
-        
-        cout << "Enter a location to mark (1-16): ";
+
+        cout << "Player " << current_player << ", enter a location to mark (1-16): ";
         cin >> input;
-        
+
         occupied = true && (board[(input - 1) / board[0].size()][(input - 1) % board[0].size()] == "X" || board[(input - 1) / board[0].size()][(input - 1) % board[0].size()] == "O");
-        
+
     } while (input < 1 || input > 16 || occupied);
-    
+
     board[(input - 1) / board[0].size()][(input - 1) % board[0].size()] = current_player;
-    
+
     if (current_player == 'X') {
         current_player = 'O';
     }
@@ -76,6 +76,21 @@ void update_board(vector<vector<string>>& board, char& current_player) {
 
 
 void output_board(vector<vector<string>> board) {
+    // loop through all indicies and output with proper format
+    for (int col = 0; col < board.size(); col++) {
+        cout << endl;
+        for (int row = 0; row < board[col].size(); row++) {
+            // add extra space if lenght one
+            if (board[col][row].length() == 1)
+                cout << board[col][row] << " \t";
+            // no extra space if length 2
+            else
+                cout << board[col][row] << "\t";
+        }
+    }
+
+    // end line for formatting
+    cout << endl;
 
 }
 
@@ -87,16 +102,21 @@ void execute_tic_tac_toe() {
     char current_player = 'X';
     bool winner = false;
 
-    do {
-        // take input
-        update_board(board, current_player);
-
-        // output board to console
+    while (true) {
         output_board(board);
+        update_board(board, current_player);
+    }
 
-        // check game board
-        winner = check_if_winner(board);
-    } while (!winner);
+//    do {
+//        // take input
+//        update_board(board, current_player);
+//
+//        // output board to console
+//        output_board(board);
+//
+//        // check game board
+//        winner = check_if_winner(board);
+//    } while (!winner);
 }
 
 
